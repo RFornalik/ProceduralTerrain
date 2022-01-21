1110bf4dca9311283b1d8311e22daf5dd27da0f2
RFornalik <45511104+RFornalik@users.noreply.github.com>
2022-01-21 18:06:21 +0100

    First Commit

---
+++ ProceduralTerrainGen/Source/ProceduralTerrainGen/ProceduralTerrainGen.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProceduralTerrainGen : ModuleRules
+{
+	public ProceduralTerrainGen(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
